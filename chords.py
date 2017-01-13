5bb62f163ebde393feae6edb9b5e242b5befbefa
jmezzanotte <johnjmezzanotte@gmail.com>
2017-01-13 15:14:14 -0800

    Added support for pentatonic scales. Still working on getting this right. We are close

--- chords.py
+++ chords.py
@@ -3,13 +3,18 @@ class Chromatic:
 
 	whole_step = 2 
 	half_step = 1 
+	flat_symbol = 'b'
+	sharp_symbol = '#'
+
 	default_flat = ['f']
+	default_flat_minor = ['c', 'd', 'g']
 
 
 	def __init__(self):
 		self.chromatic = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#',  'g', 'g#', 'a', 'a#', 'b' ]
 		self.chromatic_flats = ['c', 'db', 'd', 'eb', 'e', 'f', 'gb', 'g', 'ab', 'a', 'bb', 'b']
 	
+
 	def is_flat(self, tonic):
 
 		tonic = tonic.lower()
@@ -17,6 +22,9 @@ class Chromatic:
 		if len(tonic) == 1 :
 			if tonic in Chromatic.default_flat:
 				return True #default to sharp
+			elif type(self).__name__ == 'Minor' :
+				if tonic in Chromatic.default_flat_minor :
+					return True
 			return False
 		elif len(tonic) == 2 :
 			temp = tonic[1]
@@ -91,11 +99,10 @@ class Major(Chromatic):
 
 
 class Minor(Chromatic):
+	
 	relative_major_degree = 2
 	whole_step_index = [0,3,5,8,10]
 	half_step_index = [2,7]
-
-
 
 	def __init__(self, tonic):
 		super().__init__()
@@ -103,19 +110,68 @@ class Minor(Chromatic):
 
 	@property
 	def scale(self):
+		print("Called")
 		return super().get_scale(self.tonic, Minor.whole_step_index, Minor.half_step_index)
 
 
 	def __str__(self):
 		return ', '.join(self.scale)
 
+
+
+class Pentatonic(Chromatic):
+
+	index = 3
+
+	def __init__(self):
+		super().__init__()
+
+	def flat_five(self, scale):
+		if scale[self.__class__.index].find(self.__class__.sharp_symbol) >= 0 :
+			scale[self.__class__.index] = scale[self.__class__.index].replace(self.__class__.sharp_symbol, '')
+		else:
+			scale[self.__class__.index] = scale[self.__class__.index] + self.__class__.flat_symbol
+		return scale
+
+class MajorPentatonic(Major, Pentatonic):
 	
+	indexes = [0,1,2,4,5]
+
+	def __init__(self, tonic):
+		super().__init__(tonic)
+
+	@property
+	def scale(self):
+		# grab the parent scale
+		parent = super().scale
+		return [parent[i] for i in self.__class__.indexes]
+		
+	@property
+	def bluesy(self):
+		return super().flat_five(self.scale)
+
+
+class MinorPentatonic(Minor, Pentatonic):
+	
+	indexes = [0, 2, 3, 4, 6]
+
+	def __init__(self, tonic):
+		super().__init__(tonic)
+
+
+	@property 
+	def scale(self):
+		parent = super().scale
+		return [parent[i] for i in self.__class__.indexes]
+
+	@property
+	def bluesy(self):
+		return super().flat_five(self.scale)
 
 if __name__ == '__main__' :
 
-	a = Major('f')
-
-	print(a.scale)
+	penta = MinorPentatonic('e')
+	print(penta.bluesy)
 
 
 	
