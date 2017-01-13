7e677358baed904a32ccfa7e9fe7002507e03f9f
jmezzanotte <johnjmezzanotte@gmail.com>
2017-01-13 00:38:44 -0800

    Added support for using flats when f major is called.

--- chords.py
+++ chords.py
@@ -3,6 +3,8 @@ class Chromatic:
 
 	whole_step = 2 
 	half_step = 1 
+	default_flat = ['f']
+
 
 	def __init__(self):
 		self.chromatic = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#',  'g', 'g#', 'a', 'a#', 'b' ]
@@ -13,13 +15,14 @@ class Chromatic:
 		tonic = tonic.lower()
 
 		if len(tonic) == 1 :
-			#default to sharp
+			if tonic in Chromatic.default_flat:
+				return True #default to sharp
 			return False
-		elif len(tonic) == 2:
+		elif len(tonic) == 2 :
 			temp = tonic[1]
 			if temp == '#' :
 				return False
-			elif temp == 'b' :
+			elif temp == 'b'  :
 				return True
 		else: 
 			return 
@@ -67,14 +70,11 @@ class Chromatic:
 
 class Major(Chromatic):
 
-
-	# Your design has made the implementation of major and minor easy
 	relative_minor_degree = 5 
 	whole_step_index = [0, 2, 5, 7,9]
 	half_step_index = [4, 11]
 
 	def __init__(self, tonic):
-		# calling the parent constructor -- set up your part of the class
 		super().__init__()
 		self.tonic = tonic
 
@@ -91,82 +91,31 @@ class Major(Chromatic):
 
 
 class Minor(Chromatic):
-	pass
+	relative_major_degree = 2
+	whole_step_index = [0,3,5,8,10]
+	half_step_index = [2,7]
+
+
+
+	def __init__(self, tonic):
+		super().__init__()
+		self.tonic = tonic
+
+	@property
+	def scale(self):
+		return super().get_scale(self.tonic, Minor.whole_step_index, Minor.half_step_index)
+
+
+	def __str__(self):
+		return ', '.join(self.scale)
 
 	
-
-
-# diatonic = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#',  'g', 'g#', 'a', 'a#', 'b' ]
-# diatonic_flats = ['c', 'db', 'd', 'eb', 'e', 'f', 'gb', 'g', 'ab', 'a', 'bb', 'b']
-
-# whole_step = 2
-# half_step = 1
-# # 9 will alway land on the tonic of the relative minor
-# relative_minor = 9 
-
-
-# def _is_flat(tonic):
-
-# 	tonic = tonic.lower()
-
-# 	if len(tonic) == 1 :
-# 		#default to sharp
-# 		return False
-# 	elif len(tonic) == 2:
-# 		temp = tonic[1]
-# 		if temp == '#' :
-# 			return False
-# 		elif temp == 'b' :
-# 			return True
-# 	else: 
-# 		return 
-
-
-
-# def _get_scale_degrees(tonic):
-
-# 	tonic = tonic.lower()
-	
-
-# 	#determine which scale list tp use 
-# 	if _is_flat(tonic):
-# 		scale = diatonic_flats
-# 	elif not _is_flat(tonic):
-# 		scale = diatonic
-	
-# 	tonic = scale.index(tonic)
-
-# 	if tonic > 0 :
-# 		degrees = scale[tonic::]
-# 		degrees.extend(scale[0:tonic])
-# 		return degrees
-# 	else:
-# 		return scale[0::]
-
-
-# def major_scale(tonic):
-
-# 	scale_degrees = _get_scale_degrees(tonic)
-	
-# 	cursor = 0 
-	
-# 	while cursor < len(scale_degrees) :
-
-# 		print(scale_degrees[cursor])
-		
-# 		if cursor == 0 or cursor == 2  or cursor == 5 or cursor == 7 or cursor == 9 : 
-# 			cursor += whole_step
-# 		elif cursor == 4 or cursor == 11: 
-# 			cursor += half_step
-
-
 
 if __name__ == '__main__' :
 
+	a = Major('f')
 
-	a = Major('bb')
 	print(a.scale)
-	print(a)
 
-	print(a.relative_minor)
+
 	
